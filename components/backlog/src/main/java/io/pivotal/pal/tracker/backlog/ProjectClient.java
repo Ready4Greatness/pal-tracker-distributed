7986b78d38e98e90a9eec54eccb97ce47c7ba594
Treshauna Wright <treshauna.wright@gmail.com>
2020-04-24 02:27:20 -0500

    Circuit Breaker Lab

--- components/backlog/src/main/java/io/pivotal/pal/tracker/backlog/ProjectClient.java
+++ components/backlog/src/main/java/io/pivotal/pal/tracker/backlog/ProjectClient.java
@@ -1,12 +1,19 @@
 package io.pivotal.pal.tracker.backlog;
 
 import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.client.RestOperations;
+
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
 
 public class ProjectClient {
 
+    private final Logger LOGGER = LoggerFactory.getLogger(getClass());
     private final RestOperations restOperations;
     private final String endpoint;
+    private final Map<Long, ProjectInfo> projectCache = new ConcurrentHashMap<>();
 
     public ProjectClient(RestOperations restOperations, String registrationServerEndpoint) {
         this.restOperations = restOperations;
@@ -16,5 +23,10 @@ public class ProjectClient {
     @CircuitBreaker(name = "project", fallbackMethod = "getProjectFromCache")
     public ProjectInfo getProject(long projectId) {
         return restOperations.getForObject(endpoint + "/projects/" + projectId, ProjectInfo.class);
+    }
+
+    public ProjectInfo getProjectFromCache(long projectId, Throwable cause) {
+        LOGGER.info("Getting project with id {} from cache", projectId);
+        return projectCache.get(projectId);
     }
 }
